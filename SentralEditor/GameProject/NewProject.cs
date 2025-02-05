6e84593fb5e5e55ee0e9c499758607b3220f4651
Lars Langhelle <Larsla1@outlook.com>
2025-02-05 04:49:14 +0100

    First change
    Adding files

---
+++ SentralEditor/GameProject/NewProject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SentralEditor.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string name = "NewProject";
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if( name != value)
+                {
+
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+        private string path = $@"{Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)}\SentralProject\";
+        public string Path
+        {
+            get => path;
+            set
+            {
+                if (path != value)
+                {
+
+                    path = value;
+                    OnPropertyChanged(nameof(Path));
+                }
+            }
+        }
+    }
+
+
+}
